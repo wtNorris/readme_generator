afb18ae326fef9eaffce6594b56ec06d35b86934
wtNorb <mollyannmarieh@gmail.com>
2022-08-16 12:04:16 -0500

    initial commit again

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,55 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'A description of the project:'
+    },
+    {
+        type: 'input',
+        name: 'toc',
+        message: 'Table of Contents:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Installation instructions?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe instructions and example for use:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Add you license(s):'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Include guidelines for how to contribute?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Include a test for your application and an example of how to run the test:',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'Questions?'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
