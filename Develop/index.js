202c3b18ba6b9a46aee1faf0475913b7b9bb4320
wtNorb <mollyannmarieh@gmail.com>
2022-08-16 16:26:09 -0500

    index js questions working

--- Develop/index.js
+++ Develop/index.js
@@ -1,61 +1,105 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: 'input',
         name: 'title',
-        message: 'Project title?'
+        message: 'Project title?',
+        validate: (value) => {if(value){return true} else {return 'You must enter a project title'}}, 
     },
     {
         type: 'input',
         name: 'description',
-        message: 'A description of the project:'
+        message: 'A description of the project:',
+        validate: (value) => {if(value){return true} else {return 'You must enter a project description'}},
     },
     {
         type: 'input',
         name: 'toc',
-        message: 'Table of Contents:'
+        message: 'Table of Contents:',
+        validate: (value) => {if(value){return true} else {return 'You must enter a table of contents'}},
     },
     {
         type: 'input',
         name: 'installation',
-        message: 'Installation instructions?'
+        message: 'Installation instructions?',
+        validate: (value) => {if(value){return true} else {return 'You must enter installation instructions'}},
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Describe instructions and example for use:'
+        message: 'Describe instructions and example for use:',
+        validate: (value) => {if(value){return true} else {return 'You must enter usage instructions and an example'}},
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'Add you license(s):'
+        message: 'Add your license(s):',
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        validate: (value) => {if(value){return true} else {return 'You must enter your license(s)'}},
     },
     {
         type: 'input',
         name: 'contributing',
-        message: 'Include guidelines for how to contribute?'
+        message: 'Include guidelines for how to contribute?',
+        validate: (value) => {if(value){return true} else {return 'You must enter contribution guidelines'}},
     },
     {
         type: 'input',
         name: 'tests',
         message: 'Include a test for your application and an example of how to run the test:',
+        validate: (value) => {if(value){return true} else {return 'You must include a test for your application'}},
     },
     {
         type: 'input',
         name: 'questions',
-        message: 'Questions?'
+        message: 'Questions?',
+        validate: (value) => {if(value){return true} else {return 'You must enter a way to direct questions'}},
     },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Github username: ',
+        validate: (value) => {if(value){return true} else {return 'You must enter your username'}},
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Email: ',
+        validate: (value) => {if(value){return true} else {return 'You must enter your email'}},
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        resolve({
+            ok: true,
+            message: console.log("Congrats, your README.md file is generated.")
+        })
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init()
+.then(userInput => {
+    return generateMarkdown(userInput);
+})
+.then(rmInfo => {
+    return writeToFile(rmInfo);
+})
+.catch(err => {
+    console.log(err);
+})
